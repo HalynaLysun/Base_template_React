992b45eebed0a528a1962833c1f361aecdbe1ecd
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2025-12-03 22:42:46 +0100

    normalize temlate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import "./App.module.css";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./toDoList/toDoList.jsx";
+import HomePage from "./HomePage/HomePage.jsx";
 
 export default function App() {
   return (
